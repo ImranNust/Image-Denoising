427cdfaddcccf7f130b974be56fd28b5ebd74590
imran_muet <imran.muet@gmail.com>
2022-03-01 11:14:57 +0500

    Added a file for data processing
    Added a MATLAB script to prepare the dataset for various noise levels.

---
+++ 2019_CVPR_Paper/data_processing/generate_test_gauss_c.m
@@ -0,0 +1,7 @@
+clear; close all; clc
+
+% path of the testing directory
+test_folder_path = 'C:\Users\Imran Qureshi\Desktop\DeepLearning\Image-Denosing\data';
+
+% save_paths
+kodak_GD = 'Kodak_GD_C.mat';
