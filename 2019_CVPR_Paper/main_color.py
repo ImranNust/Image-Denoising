78f4fb48842baa9b56f9bc149cf08377ab6758e9
imran_muet <imran.muet@gmail.com>
2022-03-03 12:40:22 +0500

    Added a few details in the main file.

--- 2019_CVPR_Paper/main_color.py
+++ 2019_CVPR_Paper/main_color.py
@@ -19,25 +19,40 @@ parser = argparse.ArgumentParser(description="PyTorch Densely Connected Hierarch
 parser.add_argument("--batchSize", type=int, default=16, help="Training batch size. Default: 16")
 parser.add_argument("--nEpochs", type=int, default=100, help="Number of epochs to train for. Default: 100")
 parser.add_argument("--lr", type=float, default=1e-4, help="Learning Rate. Default=1e-4")
+parser.add_argument("--step", type=int, default=3,
+                    help="Halves the learning rate for every n epochs. Default: n=3")
 parser.add_argument("--resume", default="", type=str, help="Path to checkpoint for resume. Default: None")
-parser.add_argument("--epochs", default=10, type=int, help="Number of epochs for training. Default: 1")
+parser.add_argument("--epochs", default=100, type=int, help="Number of epochs for training. Default: 1")
 parser.add_argument("--momentum", default=0.9, type=float, help="Momentum, Default: 0.9")
 parser.add_argument("--weight-decay", "--wd", default=1e-4, type=float, help="weight decay, Default: 1e-4")
 parser.add_argument("--pretrained", default="", type=str, help="path to pretrained model. Default: None")
-parser.add_argument("--train", default="./data_processing/gaus_train_c_50_toy.h5", type=str, help="training set path.")
-parser.add_argument("--valid", default="./data_processing/gaus_val_c_50_toy.h5", type=str, help="validation set path.")
+parser.add_argument("--train", default="./data_processing/gaus_train_c_50.h5", type=str, 
+                    help="training set path.")
+parser.add_argument("--valid", default="./data_processing/gaus_val_c_50.h5", type=str, 
+                    help="validation set path.")
 parser.add_argument("--test", default= '../data/test_data/' , type=str, help="path of the test dir")
 parser.add_argument("--test_data_type", default = 'kodak', type = str, 
                    help = "We are testing our model on two types of data: kodak and BSDS300")
-parser.add_argument("--result_path", default = './Results/', type = str, help = "Path where results will be stored.")
+parser.add_argument("--result_path", default = './Results/', type = str, 
+                    help = "Path where results will be stored.")
 parser.add_argument("--gpu", default='0', help="GPU number to use when training. ex) 0,1 Default: 0")
-parser.add_argument('--input_shape', nargs = '+', default=[64, 64, 3], type=int, help='the size of the input image') 
-parser.add_argument('--model_save_path', default = "./saved_models/", type = str, help = 'Path for saving the model.' )
+parser.add_argument('--input_shape', nargs = '+', default=[64, 64, 3], type=int, 
+                    help='the size of the input image') 
+parser.add_argument('--model_save_path', default = "./saved_models/", type = str, 
+                    help = 'Path for saving the model.' )
 parser.add_argument('--NL', default = 50, type = int, help = 'Noise level: like 30 or 10 or 50' )
 
 opt = parser.parse_args()
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 print(opt)
+
+
+def adjust_learning_rate(epoch):
+    lr = opt.lr
+    for i in range(epoch // opt.step):
+        lr = lr / 2
+    return lr
+
 
 def train():
     
@@ -75,12 +90,13 @@ def train():
                                                         save_best_only = True,
                                                         save_weights_only = False,
                                                         save_freq = 'epoch')
+    lr_callback = tf.keras.callbacks.LearningRateScheduler(adjust_learning_rate)
     history = model.fit(x = input_train, y = label_train, 
                         validation_data = (input_valid, label_valid),
                         batch_size = opt.batchSize,
                         epochs = opt.epochs, 
                         shuffle = True, 
-                        callbacks = [save_model_callback])
+                        callbacks = [save_model_callback, lr_callback])
     
     return history
     
